ba3c913ba5cf1b05de5a76cf02c0900fd683395e
panxinyang <2251091259@qq.com>
2018-05-07 12:00:29 +0800

    管理员登录相关文件

---
+++ src/components/adminPage/adminSidebar1.vue
@@ -0,0 +1,17 @@
+<template>
+
+
+</template>
+
+<script>
+
+  import { URL_DATA } from '../../js/util-data'
+  export default {
+    name: "adminSidebar1",
+  }
+</script>
+
+<style scoped>
+
+
+</style>
