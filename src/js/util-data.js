b389f558f9208ad64a665a69081b4f4d519ea384
stranger469 <stranger469@icloud.com>
2018-06-04 22:06:36 +0800

    删除assert

--- src/js/util-data.js
+++ src/js/util-data.js
@@ -13,13 +13,13 @@
 // 公网ip和war包前缀
 // 39.105.58.192
 // /netEdu-1.0-SNAPSHOT
-export const LOCAL_IPADDR = '192.168.3.2:8888'
-const IPADDR = '192.168.3.2:8011'
+export const LOCAL_IPADDR = 'localhost:8888'
+const IPADDR = 'localhost:8011'
 let WAR_NAME = ''
 const PROTOCOL_HTTP = 'http://'
 const PROTOCOL_WEBSOCKET = 'ws://'
 // '0,' + 字符串
-export const SOCKET_IP = PROTOCOL_WEBSOCKET + '192.168.3.2:7117' + WAR_NAME + '/websocket'
+export const SOCKET_IP = PROTOCOL_WEBSOCKET + 'localhost:7117' + WAR_NAME + '/websocket'
 
 export const URL_DATA = {
   LOGIN: PROTOCOL_HTTP + IPADDR + WAR_NAME + '/login',
