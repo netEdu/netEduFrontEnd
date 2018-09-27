5557d010b7d3c47eeb38e2eb0fbd9d712abf26b5
Stranger469 <stranger469@icloud.com>
2018-09-27 16:14:57 +0800

    modify path

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 export default new Router({
   mode: 'history',
-  base: 'netEduFrontEnd',
+  base: '/netEduFrontEnd/',
   routes: [
     {
       path: '/',
